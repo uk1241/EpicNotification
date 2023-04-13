e1f6139b53f57a03fae2eeb54d288abcf643702f
R.Unnikrishnan <unnivr011@gmail.com>
2023-04-13 12:53:25 +0530

    new tableview cell

---
+++ EpicNotificationPage/AssetNotificationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AssetNotificationTableViewCell.swift
+//  EpicNotificationPage
+//
+//  Created by R.Unnikrishnan on 13/04/23.
+//
+
+import UIKit
+
+class AssetNotificationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
